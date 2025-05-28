16adfef6f1b4da5c50e1c05ba4662df823f0348e
AntOG96 <antoine.oger96@gmail.com>
2025-05-28 12:31:33 +0200

    started EDA integration on Streamlit

--- pages/1_📈_EDA.py
+++ pages/1_📈_EDA.py
@@ -1,4 +1,9 @@
 import streamlit as st
+import pandas as pd
+import plotly.express as px
+import ast
+
+from utils.common import df_animes
 
 st.set_page_config(page_title="Anime Recommendation Engine 🏯", layout="wide")
 
@@ -14,3 +19,35 @@ You will find visualizations and statistics about:
 
 These insights help guide the recommendation system and ensure data quality.
 """)
+
+df_animes['genre'] = df_animes['genre'].apply(ast.literal_eval)
+df_exploded = df_animes.explode('genre')
+genre_count = pd.DataFrame(df_exploded['genre'].value_counts().reset_index(name='count').head(10))
+
+count = px.bar(genre_count,
+               x='genre',
+               y='count', 
+               labels={'genre': 'Genre', 'count': 'Count'},
+               text='count',
+               title="10 most represented genres",
+               template="plotly_dark", 
+               color_discrete_sequence=px.colors.qualitative.G10)
+
+st.plotly_chart(count, use_container_width=True)
+
+st.subheader("Let's find out the average score of the 10 best ranked animes")
+
+rank = df_animes.sort_values(by='ranked').head(10)
+
+top_rank = px.bar(rank,
+                        x='title',
+                        y='score',
+                        labels={'title': 'Title', 'score': 'Score'},
+                        text='ranked',
+                        title='Top ranked animes and their average score',
+                        template="plotly_dark", 
+                        color_discrete_sequence=px.colors.qualitative.G10)
+top_rank.update_yaxes(range=[8.9,9.4])
+
+st.plotly_chart(top_rank, use_container_width=True)
+
